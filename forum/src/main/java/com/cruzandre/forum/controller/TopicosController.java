4b5e2d92c42c1845b4271d64aa0924e56a9d24cc
Andre Cruz <andrec3@gmail.com>
2020-08-31 16:49:44 -0400

    Implementation of JPA Repository

--- forum/src/main/java/com/cruzandre/forum/controller/TopicosController.java
+++ forum/src/main/java/com/cruzandre/forum/controller/TopicosController.java
@@ -1,24 +1,26 @@
 package com.cruzandre.forum.controller;
 
-import java.util.Arrays;
+
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cruzandre.forum.controller.dto.TopicoDTO;
-import com.cruzandre.forum.model.Curso;
 import com.cruzandre.forum.model.Topico;
+import com.cruzandre.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
 
 	@RequestMapping("/topicos")
 	public List<TopicoDTO> lista() {
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
 		
-		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+		List<Topico> topicos = topicoRepository.findAll();
+		return TopicoDTO.converter(topicos);
 	}
 }
---
+++ forum/src/main/java/com/cruzandre/forum/repository/TopicoRepository.java
@@ -0,0 +1,9 @@
+package com.cruzandre.forum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cruzandre.forum.model.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+}
